ae16a723a85dff492a64af131a73e2be08f4bd2b
Juan Felipe Restrepo Buitrago <jfeliperestrepob@gmail.com>
2022-08-27 15:06:25 -0500

    Adding .h file

---
+++ Mamifero.h
@@ -0,0 +1,15 @@
+//
+// Created by Usuario on 27/08/2022.
+//
+
+#ifndef CLASE3POO_MAMIFERO_H
+#define CLASE3POO_MAMIFERO_H
+
+
+class Mamifero {
+public:
+    virtual void amamantar();
+};
+
+
+#endif //CLASE3POO_MAMIFERO_H
